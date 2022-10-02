4f306182c63beb5ba52fde6c3a3959012270ad68
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-03 07:31:25 +0900

    Display todo title

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import axios from "axios";
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [todos, setTodos] = useState<any>([]);
+
   const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
+      setTodos(res.data);
     });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>Get data</button>
+      {todos.map((todo) => {
+        return <p>{todo.title}</p>;
+      })}
     </div>
   );
 }
