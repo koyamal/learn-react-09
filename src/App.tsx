8d2caedaa972f9cd51f767787b806191665a1de7
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-03 08:36:56 +0900

    Use Optional chaining "?"

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import "./styles.css";
 import { Todo } from "./Todo";
 import { TodoType } from "./type/todo";
 import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./type/user";
+
+const user: User = {
+  name: "Tom",
+  hobbies: ["movie", "game"]
+};
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -17,6 +24,7 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
       <Text color="red" fontSize="24px" />
       <button onClick={onClickFetchData}>Get data</button>
       {todos.map((todo) => {
